1cdd457136b6d1a05453887d6ac45fe81d5d9621
n2048-creative-technology <n2048-creative-technology@users.noreply.github.com>
2024-09-20 13:36:54 +0000

    deploy: 25652818957e83cdc0b553d1640c60cc0757061c

---
+++ labels.pl
@@ -0,0 +1,13 @@
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# Associate labels original text with physical files.
+
+
+1;
+
+
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# labels from external_latex_labels array.
+
+
+1;
+
