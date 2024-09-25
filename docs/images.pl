311caa96792e0d03778fc0a03a77d6ae2e9945fe
Mauricio van der Maesen de Sombreff <mauricio@n2048.com>
2024-09-25 20:30:38 +0200

    wave function collapse

---
+++ docs/images.pl
@@ -0,0 +1,11 @@
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# Associate images original text with physical files.
+
+
+$key = q/boxed{text{a}proptotext{E}};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 3.23ex; vertical-align: -0.83ex; " SRC="|."$dir".q|img1.svg"
+ ALT="\boxed{\text{a} \propto \text{E}}">|; 
+
+1;
+
