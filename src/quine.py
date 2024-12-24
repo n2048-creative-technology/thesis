0f2e1808ca8ad6d65639c8b74b1c98f2e81ea9c2
Mauricio van der Maesen de Sombreff <mauricio@n2048.com>
2024-12-24 20:17:30 +0100

    quine.py

--- src/quine.py
+++ src/quine.py
@@ -1,1 +1,2 @@
-w = 'w = {}{}{}; print(w.format(chr(39), w, chr(39)))'; print(w.format(chr(39), w, chr(39)))
+w = 'w = {}{}{}; print(w.format(chr(39), w, chr(39)))';
+print(w.format(chr(39), w, chr(39)))
